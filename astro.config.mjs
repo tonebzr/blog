5ed290090926887f40d84a708d12b772defd08e9
tonebzr <hermineinfo@proton.me>
2026-03-28 16:43:20 +0100

    feat: config i18n and root redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,38 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+	redirects: {
+        '/': '/fr/'
+    },
+    integrations: [
+        starlight({
+            title: 'TBZR Blog',
+            // On définit ici les langues supportées
+            defaultLocale: 'fr', 
+            locales: {
+                fr: {
+                    label: 'Français',
+                    lang: 'fr-FR',
+                },
+                en: {
+                    label: 'English',
+                    lang: 'en-US',
+                },
+            },
+            social: [
+                { icon: 'github', label: 'GitHub', href: 'https://github.com/tonebzr/blog' }
+            ],
+            sidebar: [
+                {
+                    label: 'Guides',
+                    translations: {
+                        en: 'Guides',
+                    },
+                    items: [
+                        { label: 'Exemple de Guide', slug: 'guides/example', translations: { en: 'Example Guide' } },
+                    ],
+                },
+            ],
+        }),
+    ],
 });
